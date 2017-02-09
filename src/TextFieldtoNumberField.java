20264ebece422166e00aa2231bb324483f8a0009
Gabriel Ernesto Mora <Gabriel Ernesto Mora>
2017-02-08 19:05:37 -0500

    Initial commit

---
+++ src/TextFieldtoNumberField.java
@@ -0,0 +1,24 @@
+import javafx.scene.control.TextField;
+
+/**
+ * Created by Gabriel on 2/8/2017.
+ */
+public class TextFieldtoNumberField {
+    public class NumberTextField extends TextField {
+        @Override
+        public void replaceText(int start, int end, String text) {
+            if (validate(text)) {
+                super.replaceText(start, end, text);
+            }
+        }
+        @Override
+        public void replaceSelection(String text) {
+            if (validate(text)) {
+                super.replaceSelection(text);
+            }
+        }
+        private boolean validate(String text) {
+            return text.matches("[0.0-9.0]*[-]?");
+        }
+    }
+}
